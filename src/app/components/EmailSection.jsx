59815933b4bbbcc2303d72ff78caaf78d92491b6
sindhueswar <sindhueswar20@gmail.com>
2023-11-06 12:36:18 +0530

    first commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -2,43 +2,19 @@
 import React, { useState } from "react";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
+import MailIcon from "../../../public/mail-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  // const [emailSubmitted, setEmailSubmitted] = useState(false);
+  // const [email, setEmail] = useState('');
+  // const [subject, setSubject] = useState('');
+  // const [message, setMessage] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
- alert("Message sent Sucessfully");
-    if (response.status === 200) {
-    alert("Message sent Sucessfully");
-      setEmailSubmitted(true);
-    }
-  };
-
+// export default sendEmail;
   return (
     <section
       id="contact"
@@ -49,12 +25,7 @@ const EmailSection = () => {
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
         </h5>
-        {/* <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
-        </p> */}
+       
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/sindhueswar">
             <Image src={GithubIcon} alt="Github Icon" />
@@ -62,9 +33,12 @@ const EmailSection = () => {
           <Link href="https://www.linkedin.com/in/sindhu-t-e/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
+          <Link href="mailto:sindhu20itr096.com">
+            <Image src={MailIcon} alt="Mail Icon" />
+          </Link>
         </div>
       </div>
-      <div>
+      {/* <div>
         {emailSubmitted ? (
           <p className="text-green-500 text-sm mt-2">
             Email sent successfully!
@@ -85,6 +59,8 @@ const EmailSection = () => {
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="abc@google.com"
+                onChange={(e) => setSubject(e.target.value)}
+
               />
             </div>
             <div className="mb-6">
@@ -101,6 +77,8 @@ const EmailSection = () => {
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Just saying hi"
+                onChange={(e) => setSubject(e.target.value)}
+
               />
             </div>
             <div className="mb-6">
@@ -115,6 +93,8 @@ const EmailSection = () => {
                 id="message"
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Let's talk about..."
+                onChange={(e) => setSubject(e.target.value)}
+
               />
             </div>
             <button
@@ -125,9 +105,147 @@ const EmailSection = () => {
             </button>
           </form>
         )}
-      </div>
+      </div> */}
     </section>
   );
 };
 
 export default EmailSection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import GithubIcon from "../../../public/github-icon.svg";
+// import LinkedinIcon from "../../../public/linkedin-icon.svg";
+// import Link from "next/link";
+// import Image from "next/image";
+// import nodemailer from 'nodemailer';
+
+// const EmailSection = () => {
+//   const [emailSubmitted, setEmailSubmitted] = useState(false);
+//   const [email, setEmail] = useState('');
+//   const [subject, setSubject] = useState('');
+//   const [message, setMessage] = useState('');
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       // Implement your email sending logic here using 'nodemailer' or your preferred email service.
+//       // Example:
+//       const transporter = nodemailer.createTransport({
+//         // Configure your email service here
+//       });
+
+//       const mailOptions = {
+//         from: 'your-email@example.com',
+//         to: email,
+//         subject: subject,
+//         text: message,
+//       };
+
+//       await transporter.sendMail(mailOptions);
+
+//       setEmailSubmitted(true);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <section
+//       id="contact"
+//       className="grid max-w-5xl align-center md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+//     >
+//       <div className="z-10">
+//         <h5 className="text-xl font-bold text-white my-2">
+//           Let&apos;s Connect
+//         </h5>
+//         <div className="socials flex flex-row gap-2">
+//           <Link href="https://github.com/sindhueswar">
+//             <Image src={GithubIcon} alt="Github Icon" />
+//           </Link>
+//           <Link href="https://www.linkedin.com/in/sindhu-t-e/">
+//             <Image src={LinkedinIcon} alt="Linkedin Icon" />
+//           </Link>
+//         </div>
+//       </div>
+//       <div>
+//         {emailSubmitted ? (
+//           <p className="text-green-500 text-sm mt-2">
+//             Email sent successfully!
+//           </p>
+//         ) : (
+//           <form className="flex flex-col" onSubmit={handleSubmit}>
+//             <div className="mb-6">
+//               <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">
+//                 Your email
+//               </label>
+//               <input
+//                 name="email"
+//                 type="email"
+//                 id="email"
+//                 required
+//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//                 placeholder="abc@google.com"
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//               />
+//             </div>
+//             <div className="mb-6">
+//               <label htmlFor="subject" className="text-white block text-sm mb-2 font-medium">
+//                 Subject
+//               </label>
+//               <input
+//                 name="subject"
+//                 type="text"
+//                 id="subject"
+//                 required
+//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//                 placeholder="Just saying hi"
+//                 value={subject}
+//                 onChange={(e) => setSubject(e.target.value)}
+//               />
+//             </div>
+//             <div className="mb-6">
+//               <label htmlFor="message" className="text-white block text-sm mb-2 font-medium">
+//                 Message
+//               </label>
+//               <textarea
+//                 name="message"
+//                 id="message"
+//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+//                 placeholder="Let's talk about..."
+//                 value={message}
+//                 onChange={(e) => setMessage(e.target.value)}
+//               />
+//             </div>
+//             <button
+//               type="submit"
+//               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+//             >
+//               Send Message
+//             </button>
+//           </form>
+//         )}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default EmailSection;
